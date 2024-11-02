1518a22c3eda0ff9b83665ec814b09f145c49adf
Bowie <rafaeldiassilv@gmail.com>
2024-11-01 23:22:19 -0300

    primeiros exercicios (simples)

---
+++ Aula02_variaveis_Exer03/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero, resultado;
+    printf("Digite um numero para ver o dobro dele: ");
+    scanf("%d", &numero);
+    printf("O dobro dele fica = %d", resultado = numero * numero);
+
+    return 0;
+}
